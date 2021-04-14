ff377f4763afb7861d807a4e718a99b57b33b3cf
termuxuser01 <termuxuser01@gmail.com>
2021-04-14 00:07:23 -0600

    File for competition objects

---
+++ comp/obj.py
@@ -0,0 +1,14 @@
+class compCar():
+    def __init__(self, price, name, description, clave, insurance):
+        self.price = price
+        self.name = name
+        self.description = description
+        self.clave = clave
+        self.insurance = insurance
+
+
+
+
+class compAvail():
+    def __init__(self, city):
+        self.city = city
