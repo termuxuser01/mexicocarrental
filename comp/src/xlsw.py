97dd6594a6e77c2d7ecb094e98c686a13c98c6aa
termuxuser01 <termuxuser01@gmail.com>
2021-04-18 20:49:03 -0600

    modified flow of creation and writing of xls files

--- comp/src/xlsw.py
+++ comp/src/xlsw.py
@@ -1,12 +1,13 @@
 import xlsxwriter
 
-def create_workbook(city, date):
-    return xlsxwriter.Workbook("{}-{}.xlsx".format(city, date))
+def create_file(name, city, date):
+    wb =  xlsxwriter.Workbook("{}-{}-{}.xlsx".format(name, city, date))
+    return wb.add_worksheet(), wb
 
-def create_worksheet(wb):
-    return wb.add_worksheet()
+def wb_close(wb):
+    wb.close()
 
-def write_to_file(ws, prices, names, hours, insurances, descriptions, claves, groups):
+def write_to_file(ws, prices, names, hours=None, insurances=None, descriptions=None, claves=None, groups=None):
     row = 0
     col = 0
 
@@ -19,6 +20,22 @@ def write_to_file(ws, prices, names, hours, insurances, descriptions, claves, gr
     row += 1
     col = 0
 
+    if(hours == None):
+        hours = return_NA(names)
+
+    if(insurances == None):
+        insurances = return_NA(names)
+    
+    if(descriptions == None):
+        descriptions = return_NA(names)
+    
+    if(claves == None):
+        claves = return_NA(names)
+    
+    if(groups == None):
+        groups = return_NA(names)
+    
+
     for n, p, h, d, c, g, s in zip(names, prices, hours, descriptions, claves, groups, insurances):
         ws.write(row, col, n)
         col += 1
@@ -26,17 +43,30 @@ def write_to_file(ws, prices, names, hours, insurances, descriptions, claves, gr
         col += 1
         ws.write(row, col, h)
         col += 1
-        ws.write(row, col, d)
+        if(type(d) == list):
+            ws.write(row, col, create_string(d))
+        else:
+            ws.write(row, col, d)
         col += 1
         ws.write(row, col, c)
         col += 1
         ws.write(row, col, g)
         col += 1
-        ws.write(row, col, create_insurance_string(s))
+        ws.write(row, col, create_string(s))
         row += 1
         col = 0
 
-def create_insurance_string(i):
+def return_NA(names):
+    temp = list()
+    
+    for i in range(len(names)):
+            temp.append("N/A")
+
+    return temp
+
+def create_string(i):
+    if(i == "N/A"):
+        return i
     final = str()
     for ins in i:
         final += "*{}\n".format(ins)
